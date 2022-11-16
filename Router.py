7a0fca06ef71db0e6471bea2a55d7db4f71d3051
Hojin Ryu <robert.ryu01@gmail.com>
2022-11-15 23:09:58 -0300

    update

--- Router.py
+++ Router.py
@@ -1,26 +1,25 @@
-import IP
-import MAC
+from IP import IP
+from MAC import MAC
 from NetworkEntityAbstraction import NetworkEntity
 from RouterTableInfo import RouterTableInfo
-import Rede
 
 class Router(NetworkEntity): 
     
-    rede = Rede.redeGlobal
-
     def __init__(self, name: str, numberOfPorts: int):
         self.name = name
-        self.numberOfPorts = numberOfPorts
+        self.numberOfPorts = int(numberOfPorts)
         self.portsInUse = 0
+        self.mask = 0
         self.ports = {}
         self.routerTableInfos = []
 
     def protocoloDeRede(self, protocol):
         return
 
-    def addNewIPAndMac(self, ip: IP.IP, mac: MAC.MAC):
+    def addNewIPAndMac(self, ip: IP, mac: MAC):
         if self.portsInUse < self.numberOfPorts:
-            self.ports[ip.ipStr] = mac
+            self.mask = ip.maskInt
+            self.ports[ip.ipStr] = mac.macStr
             self.portsInUse += 1
 
         else:
@@ -29,14 +28,14 @@ class Router(NetworkEntity):
     def updateNewRouterTable(self, rT: RouterTableInfo):
         self.routerTableInfos.append(rT)
 
-    def isMyIP(self, ip:IP.IP) -> bool:
+    def isMyIP(self, ip:IP) -> bool:
         for thisIp in self.ports: 
             if thisIp == ip.ipStr:
                 return True
 
         return False
 
-    def isMyMAC(self, mac:MAC.MAC) -> bool:
+    def isMyMAC(self, mac:MAC) -> bool:
         for thisMac in self.ports.values(): 
             if thisMac == mac.macStr:
                 return True
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from Reader import Reader
+from ARP import ARPRequest
+from ARP import ARPReply
 # n1 = Nodo(ip = 123, mac = 234)
 
 # n1.executaProtocoloDeRede(ARP(IP("12.12.3.123",12), n1.ip, "request"))
 
 reader = Reader()
 reader.read("topologia.txt")
+
+for node in reader.nodes:
+  print(node.name)
+
+for router in reader.routers:
+  print(router.name)
+
+src = reader.nodes[0]
+dst = reader.nodes[1]
+
+# print(reader.rede.dicionarioDeRedes)
+for key in reader.rede.dicionarioDeRedes.keys():
+  print("key: ", key, reader.rede.dicionarioDeRedes[key])
+# arpRequest = ARPRequest(src.ip, dst.ip, src.mac)
+# reader.nodes[0].arpRequest()
