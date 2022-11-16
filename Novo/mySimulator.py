760e055b23d608eb1dfa94c524d463978f08d740
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2022-11-16 20:52:09 -0300

    portaDeRedeEntrada

--- Novo/mySimulator.py
+++ Novo/mySimulator.py
@@ -1,8 +1,17 @@
 # arpTable é matriz onde as linhas tem 2 valores, primeira[0] é ip a segunda[1] é mac
 # router table é matriz onde as linhas são [0] = ip, [1] = nextHop(IP, mas se for só zero ele está na rede), [2] = porta 
 
+
 class entity():
     def __init__(self, ip, mac, arpTable = [], routerTable = []):
         self.ip = ip
         self.mac = mac
         self.arpTable = arpTable
+
+
+
+redes = []
+#isso esta fora da classe
+def portaDeRedeEntrada(entidade , protocol):
+    # testar se é arp ou icmp
+    
